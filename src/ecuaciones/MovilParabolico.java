3114be511428a7164e53a82de93600e74ed09bf6
jibaru <xxnacho1997xx@gmail.com>
2019-10-26 23:11:45 -0500

    Clase del movil parabolico

--- src/ecuaciones/MovilParabolico.java
+++ src/ecuaciones/MovilParabolico.java
@@ -1,75 +1,80 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ecuaciones;
 
+import static java.lang.Math.PI;
 import static java.lang.Math.cos;
 import static java.lang.Math.pow;
 import static java.lang.Math.sin;
-import javax.swing.JOptionPane;
+import static java.lang.Math.tan;
 
-/**
- *
- * @author Administrador
- */
 public class MovilParabolico {
     
-    final double gravedad = 9.8;
-    double vInicial;
-    double angulo; 
+    final double gravedad = 9.8; 
     double Vx, Vy;
     double Dx, Hy, Dmax, Hmax;
-    double tiempo;
-    
-    public MovilParabolico(){
-        
-    }
-    
-    public void Datos(){
-    tiempo = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el tiempo: "));
-    vInicial = Float.parseFloat(JOptionPane.showInputDialog("Ingrese la velocidad inicial: "));
-    angulo = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el angulo: "));
-    }
+    double tiempo, tVuelo,tHmax;
+    double tray;
 
     public void Velocidades( double ang, double vIni){ 
-        Vx = vIni*cos(ang*Math.PI/180);
-        Vy = vIni*sin(ang*Math.PI/180);
+        Vx = vIni*cos(ang*PI/180);
+        Vy = vIni*sin(ang*PI/180);
     }
     
     public double velX(double ang, double vIni){
-        Vx = vIni*cos(ang*Math.PI/180);
+        Vx = vIni*cos(ang*PI/180);
         return Vx;
     }
     
+    public double velY(double ang, double vIni){
+        Vy = vIni*cos(ang*PI/180);
+        return Vy;
+    }
+
+    public void Distancias(double ang, double vIni, double tiempo){
+        Dx = vIni*cos(ang*PI/180)*tiempo;
+        Dmax = pow(vIni, 2)*sin(2*ang*PI/180);
+    }
     
-
-    public void Altura(){
-        if(Dx > Dmax){
-            Hy = vInicial*sin(angulo*Math.PI/180)*tiempo - 0.5*gravedad*pow(tiempo, 2);
-        }
-        else{
-            Hy = vInicial*sin(angulo*Math.PI/180)*tiempo + 0.5*gravedad*pow(tiempo, 2);
-        }
-        Hmax = pow( vInicial*sin(angulo*Math.PI/180), 2)/2*gravedad;
+    public double distX(double ang, double vIni, double tiempo){
+        Dx = vIni*cos(ang*PI/180)*tiempo;
+        return Dx;
     }
-
-    public void Distancia(){
-        Dx = vInicial*cos(angulo*Math.PI/180)*tiempo;
-        Dmax = pow( vInicial, 2)*sin(2*angulo*Math.PI/180)/gravedad;
+    public double distMax(double ang, double vIni, double tiempo){
+        Dmax = pow(vIni, 2)*sin(2*ang*PI/180);
+        return Dmax;
     }
-
-    public void Resultados(){
-        System.out.println("El tiempo de ejecucion es: "+tiempo);
-        System.out.println("La velocidad en el eje x es: "+Vx);
-        System.out.println("La velocidad en el eje y es: "+Vy);
-        System.out.println("La distancia es: "+Dx);
-        System.out.println("La altura es: "+Hy);
-        System.out.println("La altura maxima posible es: "+Hmax);
-        System.out.println("El alcance: "+Dmax);
+    
+    public void Tiempos(double vIni, double ang){
+        tHmax = vIni*sin(ang*PI/180)/gravedad;
+        tVuelo = 2*vIni*sin(ang*PI/180)/gravedad;
+    }
+    
+    public double tiempoHmax(double vIni, double ang){
+        tHmax = vIni*sin(ang*PI/180)/gravedad;
+        return tHmax;
+    }
+    
+    public double tiempoVuelo(double vIni, double ang){
+        tVuelo = 2*vIni*sin(ang*PI/180)/gravedad;
+        return tVuelo;
+    }
+    
+    public void Alturas(double ang, double vIni, double tiempo){
+        Hy = Vy*tiempo - 0.5*gravedad*pow(tiempo, 2);
+        Hmax = (pow(sin(ang*PI/180)*vIni, 2))/(2*gravedad);
+    }
+    
+    public double altura(double ang, double vIni, double tiempo){
+        Hy = Vy*tiempo - 0.5*gravedad*pow(tiempo, 2);
+        return Hy;
+    }
+    public double altMax(double ang, double vIni){
+        Hmax = (pow(sin(ang*PI/180)*vIni, 2))/(2*gravedad);
+        return Hmax;
+    }
+    
+    public double Trayectoria(double vIni, double ang, double tiempo){
+        double x = vIni*cos(ang*PI/180)*tiempo;
+        tray = x*tan(ang*PI/180)-0.5*gravedad*pow(x/(vIni*cos(ang*PI/180)), 2);
+        return tray;
     }
 }
-
-
-
